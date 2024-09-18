1b9adbc6079164c68eb35d4eb735e6fa9e7d0fa4
Alejandro Gomez <agomez03@uafs.edu>
2024-09-17 21:37:50 -0500

    update

--- Server.java
+++ Server.java
@@ -1,26 +1,28 @@
+
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.File;
-import java.io.FileReader;
 
-public class Server{
+public class Server {
+
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private int portNo;
 
-    public Server(int portNo){
-        this.portNo=portNo;
+    public Server(int portNo) {
+        this.portNo = portNo;
     }
 
-    private void processConnection() throws IOException{
+    private void processConnection() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-      /*  //*** Application Protocol *****
+        /*  //*** Application Protocol *****
         String buffer = in.readLine();
         while(buffer.length() != 0){
             System.out.println(buffer);
@@ -33,7 +35,7 @@ public class Server{
 
         in.close();
         out.close();
-        */
+         */
         String request = in.readLine();
         if(request != null && !request.isEmpty()){
             System.out.println("The request: " + request);
@@ -49,7 +51,7 @@ public class Server{
             }
         
 
-        File file = new File("/home/student/projects/assignment-1-AJGomez311-1/docroot/");  
+        File file = new File("/home/student/projects/assignment-1-AJGomez311-1/docroot/" + requestedFile);  
         if(file.exists() && !file.isDirectory()){
             out.printf("HTTP/1.1 200 OK\n");
             out.printf("Content-Length: " + file.length() + "\n");
@@ -74,12 +76,12 @@ public class Server{
 
     }
 
-    public void run() throws IOException{
+    public void run() throws IOException {
         boolean running = true;
-       
+
         serverSocket = new ServerSocket(portNo);
-        System.out.printf("Listen on Port: %d\n",portNo);
-        while(running){
+        System.out.printf("Listen on Port: %d\n", portNo);
+        while (running) {
             clientSocket = serverSocket.accept();
             //** Application Protocol
             processConnection();
@@ -87,7 +89,8 @@ public class Server{
         }
         serverSocket.close();
     }
-    public static void main(String[] args0) throws IOException{
+
+    public static void main(String[] args0) throws IOException {
         Server server = new Server(8080);
         server.run();
     }
